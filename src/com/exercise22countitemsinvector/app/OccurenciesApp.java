10b738aaa65e6178e835873a2d68ae9ea4e42ecc
devjosegomez <devjosegomez@gmail.com>
2019-10-23 13:39:28 -0500

    Example of how many times each element occurs on a Hash Map

--- src/com/exercise22countitemsinvector/app/OccurenciesApp.java
+++ src/com/exercise22countitemsinvector/app/OccurenciesApp.java
@@ -1,9 +1,38 @@
 package com.exercise22countitemsinvector.app;
 
+import java.util.HashMap;
+import java.util.Random;
+
 public class OccurenciesApp {
 
 	public static void main(String[] args) {
+		final int ELEMENTS=100;
 		
+		Random numbers = new Random(System.nanoTime());
+		int[] myArray = new int[ELEMENTS];
+		HashMap<Integer, Integer> myHash = new HashMap<Integer, Integer>();
+		
+		for(int i = 0; i<ELEMENTS; i++) {
+			myArray[i] = numbers.nextInt(11);
+		}
+		
+		int occurences = 0;
+		for(int  element : myArray) {
+			occurences = 0;
+			if(myHash.containsKey(element)) {
+				occurences = myHash.get(element);
+				occurences++;
+				myHash.put(element, occurences);
+			}else {
+				myHash.put(element, 1);
+			}
+		}
+		
+		System.out.println(myHash);
+		
+		for(int  i : myArray) {
+			System.out.print(" " + i);
+		}
 
 	}
 
